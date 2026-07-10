2b4b8127fbcdfd7d9f914eaa91ad531a901b734f
AXL <121584263+06Kg@users.noreply.github.com>
2026-07-10 02:28:29 +0000

    fix lavalink node

--- config.js
+++ config.js
@@ -26,9 +26,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "lava-v4.ajieee.com", //- The host name or IP of the lavalink server.
+			host: "lavalink.pantheralps.cc", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
-			password: "https://dsc.gg/ajieee", //- The password of the lavalink server.
+			password: "pantheralps.cc", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
