b6b8ce622b140330d1cea9ef583ac9a3fdc45944
AXL <121584263+06Kg@users.noreply.github.com>
2026-07-10 01:37:52 +0000

    deploy bot

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: "1463170775581462756", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTUyNDkzNDc0NjQyODI4MDg1Mg.GqbFwd.nr5TYARXJNAdAKFZ4igGgS8VclmGnsojR0MUkY", //- Bot's Token
+	clientId: process.env.clientId || "1524934746428280852", //- ID of the bot
+	clientSecret: process.env.clientSecret || "jSmbJ3zrTM3qLBx7THRzXaeaZSBzBK1g", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
@@ -26,8 +26,8 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
+			host: "127.0.0.1", //- The host name or IP of the lavalink server.
+			port: 8080, // The port that lavalink is listening to. This must be a number!
 			password: "", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
